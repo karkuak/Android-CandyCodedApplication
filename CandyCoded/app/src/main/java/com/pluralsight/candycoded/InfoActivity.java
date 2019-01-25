123a7935eb7730cf7a1ea12203dacf4e1aea809e
karthik-sivakumar <43709640+karkuak@users.noreply.github.com>
2019-01-24 22:44:52 -0500

    project section 3

--- CandyCoded/app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ CandyCoded/app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -30,11 +30,9 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     public void createMapIntent(View view){
        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uriAddress);
-
-        mapIntent.setPackage("com.google.android.apps.map");
-
-        if(mapIntent.resolveActivity(getPackageManager())!=null){
+       Intent mapIntent = new Intent(Intent.ACTION_VIEW,uriAddress);
+       mapIntent.setPackage("com.google.android.apps.map");
+       if(mapIntent.resolveActivity(getPackageManager())!=null){
             startActivity(mapIntent);
         }
     }
